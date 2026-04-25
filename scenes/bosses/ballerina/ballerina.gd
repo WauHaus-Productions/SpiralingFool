30203de992dfd7a209dab915a2035e62a9761cee
FraMaffezzoli <francesco.maffezzoli@gmail.com>
2026-04-25 15:26:30 +0200

    damage dealt/received now don't sacle with frame rate

--- scenes/bosses/ballerina/ballerina.gd
+++ scenes/bosses/ballerina/ballerina.gd
@@ -8,7 +8,7 @@ signal player_hit
 @export var acceleration = 20
 @export var dash_prob = 0.7
 @export var turn_speed = 1
-@export var cerchio_dmg :float
+@export var cerchio_dmg_per_sec :float
 @onready var audio_shoot = $Shoot
 @onready var audio_dash = $Dash
 @onready var audio_defeat = $Defeat
@@ -56,11 +56,11 @@ func _process(delta: float) -> void:
 				
 				animation_sprites.rotate((curr_velocity/max_vel) * 0.5)
 				if ramp_up == 1:
-					curr_velocity = curr_velocity + acceleration
+					curr_velocity = curr_velocity + acceleration*delta
 					if curr_velocity > max_vel:
 						ramp_up = 0
 				if ramp_up == 0:
-					curr_velocity = curr_velocity - 20
+					curr_velocity = curr_velocity - acceleration*2*delta
 					if curr_velocity < base_velocity:
 						idle_direction = get_random_direction()
 						action = "idle"
@@ -77,40 +77,42 @@ func _process(delta: float) -> void:
 
 func _on_do_something_timeout() -> void:
 	var what_to_do = rng.randf_range(0, 1)
-	if what_to_do < dash_prob:
-		direction = (player.global_position - global_position).normalized()
-		curr_velocity = 5
-		ramp_up = 1
-		self.velocity = direction*curr_velocity
-		
-		animation_sprites.play("charge")
-		audio_dash.play()
-		action = "charge"
+	if player != null:
+		if what_to_do < dash_prob:
+			direction = (player.global_position - global_position).normalized()
+			curr_velocity = base_velocity*2;
+			ramp_up = 1
+			self.velocity = direction*curr_velocity
+			
+			animation_sprites.play("charge")
+			audio_dash.play()
+			action = "charge"
 
-	else:
-		direction = (player.global_position - global_position).normalized()
-		action = "shoot_windup"
-		curr_velocity = 0
-		if direction.x < 0:
-			animation_sprites.play("shoot_windup_left")
-			animation_sprites.animation_looped
 		else:
-			animation_sprites.play("shoot_windup_right")
-		await get_tree().create_timer(0.5).timeout
-		direction = (player.global_position - global_position).normalized() 
-		cerchio = cerchio_scene.instantiate()
-		add_child(cerchio)
-		cerchio.connect("player_hitted", _on_player_hitted)
-		cerchio.player = player
-		cerchio.global_position = self.global_position
-		cerchio.direction = direction
-		cerchio.player_position = player.global_position
-		cerchio.boss_position = self.global_position
-		audio_shoot.play()
-		action = "shoot"
+			direction = (player.global_position - global_position).normalized()
+			action = "shoot_windup"
+			curr_velocity = 0
+			if direction.x < 0:
+				animation_sprites.play("shoot_windup_left")
+				animation_sprites.animation_looped
+			else:
+				animation_sprites.play("shoot_windup_right")
+			await get_tree().create_timer(0.5).timeout
+			direction = (player.global_position - global_position).normalized() 
+			cerchio = cerchio_scene.instantiate()
+			add_child(cerchio)
+			cerchio.connect("player_hitted", _on_player_hitted)
+			cerchio.player = player
+			cerchio.global_position = self.global_position
+			cerchio.direction = direction
+			cerchio.player_position = player.global_position
+			cerchio.boss_position = self.global_position
+			audio_shoot.play()
+			action = "shoot"
 		
-func _on_player_hitted():
-	player.take_damage(cerchio_dmg)
+func _on_player_hitted(delta_ball):
+	if player != null:
+		player.take_damage(cerchio_dmg_per_sec*delta_ball)
 	
 func take_damage(damage) -> void:
 	hp -= damage
--- scenes/bosses/ballerina/cerchio.gd
+++ scenes/bosses/ballerina/cerchio.gd
@@ -3,7 +3,7 @@ extends Area2D
 signal player_hitted
 var rng = RandomNumberGenerator.new()
 
-@export var speed = rng.randf_range(5, 15)
+@export var speed = rng.randf_range(8, 11)
 @export var cooldown = 0.2
 @onready var audio_collision = $Collision
 @onready var audio_in_flight = $In_flight
@@ -22,7 +22,7 @@ var despawned = false
 
 var circular_offset
 var circular_offset_rotated
-var radius = rng.randf_range(5, 10) # Radius of the circular motion
+var radius = rng.randf_range(5, 7) # Radius of the circular motion
 var angular_speed = rng.randf_range(50, 70)
 # Angular speed for circular motion (radians per second)
 var time_passed = 0.0 # Keeps track of elapsed time
@@ -37,11 +37,11 @@ func _process(delta: float) -> void:
 	time_passed += delta
 	circular_offset = Vector2(cos(time_passed * angular_speed), sin(time_passed * angular_speed)) * radius
 	circular_offset_rotated = circular_offset.rotated(direction.angle())
-	self.position += direction*speed + circular_offset_rotated
+	self.position += direction*speed*delta*130 + circular_offset_rotated
 	distance_from_player = player_position.distance_to(global_position)
 	distance_from_boss = boss_position.distance_to(global_position)
 	if self.overlaps_body(self.player):
-		emit_signal("player_hitted")
+		emit_signal("player_hitted", delta)
 			
 	if (distance_from_player < 50):
 		start_cooldown = true
